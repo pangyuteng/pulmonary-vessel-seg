5d9ac8b1c82e86aa683498a35990f221dd754642
pangyuteng <pangyuteng@gmail.com>
2023-01-26 22:37:38 +0000

    reverted, stl should be generated using full rez nifti mask

--- methods/viz.sh
+++ methods/viz.sh
@@ -8,15 +8,11 @@ export DOWN_FILE=${OUTPUT_NIFTI_FOLDER}/img-downsampled.nii.gz
 python viz_utils.py downsample $IMG_FILE $DOWN_FILE False
 
 export VSL_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski.nii.gz
-export DOWN_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski-downsampled.nii.gz
-export STL_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski-down.stl
+export STL_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski.stl
 
-python viz_utils.py downsample $VSL_FILE $DOWN_FILE True
-python viz_utils.py stl $DOWN_FILE $STL_FILE
+python viz_utils.py stl $VSL_FILE $STL_FILE
 
 export VSL_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal.nii.gz
-export DOWN_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal-downsampled.nii.gz
 export STL_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal.stl
 
-python viz_utils.py downsample $VSL_FILE $DOWN_FILE True
-python viz_utils.py stl $DOWN_FILE $STL_FILE
+python viz_utils.py stl $VSL_FILE $STL_FILE
