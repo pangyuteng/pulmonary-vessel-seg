6d4b139c5960caa545a78a662fa5f1491c3f6724
pangyuteng <pangyuteng@gmail.com>
2023-01-27 01:45:58 +0000

    wip 3js

--- methods/viz.sh
+++ methods/viz.sh
@@ -2,10 +2,16 @@
 
 export OUTPUT_NIFTI_FOLDER=$1
 
+
+### downsample image
+
 export IMG_FILE=${OUTPUT_NIFTI_FOLDER}/img.nii.gz
 export DOWN_FILE=${OUTPUT_NIFTI_FOLDER}/img-downsampled.nii.gz
 
 python viz_utils.py downsample $IMG_FILE $DOWN_FILE False
+
+
+### create stl
 
 export VSL_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski.nii.gz
 export STL_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski.stl
@@ -16,3 +22,17 @@ export VSL_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal.nii.gz
 export STL_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal.stl
 
 python viz_utils.py stl $VSL_FILE $STL_FILE
+
+#### create downsampled stl
+
+export VSL_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski.nii.gz
+export DOWN_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski-downsampled.nii.gz
+export STL_FILE=${OUTPUT_NIFTI_FOLDER}/knopczynski-downsampled.stl
+python viz_utils.py downsample $VSL_FILE $DOWN_FILE True
+python viz_utils.py stl $DOWN_FILE $STL_FILE
+
+export VSL_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal.nii.gz
+export DOWN_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal-downsampled.nii.gz
+export STL_FILE=${OUTPUT_NIFTI_FOLDER}/wasserthal-downsampled.stl
+python viz_utils.py downsample $VSL_FILE $DOWN_FILE True
+python viz_utils.py stl $DOWN_FILE $STL_FILE
