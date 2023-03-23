05a9270d791507174bde54c3c94586addb2b602f
pteng <pteng@medqia.com>
2023-03-23 14:07:33 -0700

    added erosion

--- methods/remove_non_lung.py
+++ methods/remove_non_lung.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+from scipy import ndimage
 import SimpleITK as sitk
 
 lung_file = sys.argv[1]
@@ -10,6 +12,7 @@ vsl_obj = sitk.ReadImage(vessel_file)
 lung = sitk.GetArrayFromImage(lung_obj)
 vsl = sitk.GetArrayFromImage(vsl_obj)    
 
+lung = ndimage.binary_erosion(lung,iterations=5).astype(np.uint8)
 vsl[lung==0]=0
 vsl = vsl.astype(np.uint8)
 vsl_obj = sitk.GetImageFromArray(vsl)
