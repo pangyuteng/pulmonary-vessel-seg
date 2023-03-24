e08f0080127ebb8c284c6aeab9d3620dca0ac8ef
pteng <pteng@medqia.com>
2023-03-24 13:42:33 -0700

    added notimplementederror

--- methods/blood_volume/estimate_blood_volume.py
+++ methods/blood_volume/estimate_blood_volume.py
@@ -4,14 +4,50 @@ import SimpleITK as sitk
 from skimage.morphology import skeletonize
 from skimage.segmentation import watershed
 
+def resample_img(itk_image, out_spacing, is_label=False):
+    
+    # Resample images to 2mm spacing with SimpleITK
+    original_spacing = itk_image.GetSpacing()
+    original_size = itk_image.GetSize()
+
+    out_size = [
+        int(np.round(original_size[0] * (original_spacing[0] / out_spacing[0]))),
+        int(np.round(original_size[1] * (original_spacing[1] / out_spacing[1]))),
+        int(np.round(original_size[2] * (original_spacing[2] / out_spacing[2])))]
+
+    resample = sitk.ResampleImageFilter()
+    resample.SetOutputSpacing(out_spacing)
+    resample.SetSize(out_size)
+    resample.SetOutputDirection(itk_image.GetDirection())
+    resample.SetOutputOrigin(itk_image.GetOrigin())
+    resample.SetTransform(sitk.Transform())
+    resample.SetDefaultPixelValue(itk_image.GetPixelIDValue())
+
+    if is_label:
+        resample.SetInterpolator(sitk.sitkNearestNeighbor)
+    else:
+        resample.SetInterpolator(sitk.sitkLinear)
+
+    return resample.Execute(itk_image)
+
 def main(image_file,mask_file,qia_file):
     
     image_obj = sitk.ReadImage(mask_file)
     mask_obj = sitk.ReadImage(mask_file)
+
+    # https://pubmed.ncbi.nlm.nih.gov/23656466/
+    # https://www.atsjournals.org/doi/full/10.1164/rccm.201301-0162OC
+    # https://www.atsjournals.org/doi/suppl/10.1164/rccm.201301-0162OC?role=tab
+
+    out_spacing = [0.625,0.625,0.625]
+    image_obj = resample_img(image_obj, out_spacing, is_label=False)
+    mask_obj = resample_img(mask_obj, out_spacing, is_label=True)
     spacing = mask_obj.GetSpacing()
     origin = mask_obj.GetOrigin()
     direction = mask_obj.GetDirection()
     vsl_mask = sitk.GetArrayFromImage(mask_obj)
+    
+    raise NotImplementedError("different from scale-space-particles")
 
     arr_list = [np.zeros_like(vsl_mask)]
     '''
@@ -21,7 +57,7 @@ def main(image_file,mask_file,qia_file):
     we can use FWHM as diameter.
     so if we have a sigma of 1mm, then diameter would be 2.355*1 mm
     '''
-
+    # np.linspace(0,6,5) 
     for x_mm in np.arange(0.2,4,0.3):
         print(f'sigma: {x_mm}') 
         # since the image is not 1mm isotropic
