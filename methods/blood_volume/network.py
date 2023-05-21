d689f00880b7a58561f0e9e9f25d71525e93d3eb
pteng <pteng@medqia.com>
2023-05-21 13:28:49 -0700

    wip network

--- methods/blood_volume/network.py
+++ methods/blood_volume/network.py
@@ -8,6 +8,7 @@ from skimage.measure import label, regionprops
 from scipy import ndimage
 import networkx as nx
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 mask_file = sys.argv[1]
 g_file = "test.gpickle"
@@ -22,51 +23,51 @@ if not os.path.exists(g_file):
     print('intersection...')
     weights = np.ones((3,3,3))
     intersection = ndimage.convolve(skeleton,weights) > 3
-    intersection = label(intersection)
-    intersection = intersection.astype(np.int16)
 
-    print('label...')
-    branch = np.copy(skeleton)
-    branch[intersection>0]=0
-    branch = label(branch)
-    branch = branch.astype(np.int16)
-
+    # following methodology by Chapman 2016 https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4547695
     G=nx.Graph()
-    indices = zip(*np.where(skeleton==1))
-
-    for x,y,z in indices:
-        try:
-            idx = branch[x,y,z]
-            #G.add_node(idx,pos=(x,y,z))
-            G.add_node(idx,pos=(x,y))
-            for ox in [-1,1]:
-                for oy in [-1,1]:
-                    for oz in [-1,1]:
+    for x,y,z in tqdm(np.argwhere(skeleton==1)):
+        #idx = np.ravel_multi_index([x,y,z],skeleton.shape)
+        idx = (x,y,z)
+        G.add_node(idx,pos=(x,y))
+        for ox in [-1,1]:
+            for oy in [-1,1]:
+                for oz in [-1,1]:
+                    try:
                         mx,my,mz = x+ox,y+oy,z+oz
-                        nidx = branch[mx,my,mz]
-                        if nidx == 0:
+                        #midx = np.ravel_multi_index([mx,my,mz],skeleton.shape)
+                        midx = mx,my,mz
+                        if skeleton[mx,my,mz]==0:
                             continue
-                        #G.add_node(nidx,pos=(mx,my,mz))
-                        G.add_node(nidx,pos=(mx,my))
-                        if idx == nidx:
-                            weight = 0
-                        else:
-                            weight = 1
-                        G.add_edge(idx,nidx,weight=weight)
-                        print(weight,idx,nidx,x,y,z,mx,my,mz)
-
-        except:
-            traceback.print_exc()
+                        G.add_node(midx,pos=(mx,my))
+                        G.add_edge(idx,midx)
+                    except:
+                        traceback.print_exc()
         
     nx.write_gpickle(G, g_file)
+
 G = nx.read_gpickle(g_file)
+
 print(G)
 pos=nx.get_node_attributes(G,'pos')
-nx.draw(G,pos)
-plt.savefig('ok.png')
+nx.draw(G,pos,node_size=1,alpha=0.5)
+plt.savefig('graph.png')
 
+mask_obj = sitk.ReadImage(mask_file)
+vsl_mask = sitk.GetArrayFromImage(mask_obj)
+mygraph = np.zeros_like(vsl_mask)
+for item in tqdm(G.degree):
+    k,v = item
+    x,y,z = k
+    mygraph[x,y,z]=v+1
+print(np.unique(mygraph))
+graph_obj = sitk.GetImageFromArray(mygraph)
+graph_obj.CopyInformation(mask_obj)
+sitk.WriteImage(graph_obj,'graph.nii.gz')
 
 '''
+
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4547695
 
 https://www.nature.com/articles/s41598-023-31470-6
 
