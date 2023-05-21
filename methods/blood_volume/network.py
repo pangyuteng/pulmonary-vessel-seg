0d5dd17f1d8998fca0a40ca2a8bce11e36d38893
pteng <pteng@medqia.com>
2023-05-21 13:33:44 -0700

    wip network

--- methods/blood_volume/network.py
+++ methods/blood_volume/network.py
@@ -29,7 +29,7 @@ if not os.path.exists(g_file):
     for x,y,z in tqdm(np.argwhere(skeleton==1)):
         #idx = np.ravel_multi_index([x,y,z],skeleton.shape)
         idx = (x,y,z)
-        G.add_node(idx,pos=(x,y))
+        G.add_node(idx,pos=(z,x))
         for ox in [-1,1]:
             for oy in [-1,1]:
                 for oz in [-1,1]:
@@ -39,7 +39,7 @@ if not os.path.exists(g_file):
                         midx = mx,my,mz
                         if skeleton[mx,my,mz]==0:
                             continue
-                        G.add_node(midx,pos=(mx,my))
+                        G.add_node(midx,pos=(mz,mx))
                         G.add_edge(idx,midx)
                     except:
                         traceback.print_exc()
@@ -48,22 +48,31 @@ if not os.path.exists(g_file):
 
 G = nx.read_gpickle(g_file)
 
-print(G)
-pos=nx.get_node_attributes(G,'pos')
-nx.draw(G,pos,node_size=1,alpha=0.5)
-plt.savefig('graph.png')
-
 mask_obj = sitk.ReadImage(mask_file)
 vsl_mask = sitk.GetArrayFromImage(mask_obj)
 mygraph = np.zeros_like(vsl_mask)
+node_colorcolor = []
 for item in tqdm(G.degree):
     k,v = item
     x,y,z = k
-    mygraph[x,y,z]=v+1
-print(np.unique(mygraph))
+    if v == 0:
+        c = (0,1,0)
+    elif v == 1:
+        c = (1,0,0)
+    else:
+        c = (0,0,1)
+
+    mygraph[x,y,z] = v+1
+    node_colorcolor.append(c)
+
 graph_obj = sitk.GetImageFromArray(mygraph)
 graph_obj.CopyInformation(mask_obj)
 sitk.WriteImage(graph_obj,'graph.nii.gz')
+
+pos=nx.get_node_attributes(G,'pos')
+nx.draw(G,pos,node_size=1,alpha=0.5,node_colorcolor=node_colorcolor)
+plt.savefig('graph.png')
+
 
 '''
 
