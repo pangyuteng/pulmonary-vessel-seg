55fb1b42a651f19526b330dbda562bf8e5526e78
pteng <pteng@medqia.com>
2023-05-22 13:48:19 -0700

    added agg

---
+++ methods/blood_volume/test/agg_vessel12.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pandas as pd
+import json
+import shutil
+from pathlib import Path
+
+def main(myfolder):
+    json_file_list = sorted(list(Path(myfolder).rglob("*.json")))
+    mylist = []
+    for json_file in json_file_list:
+        idx = os.path.basename(os.path.dirname(json_file))
+        with open(json_file,'r') as f:
+            mydict = json.loads(f.read())
+        mydict['idx']=idx
+        mylist.append(mydict)
+    cols = ['idx','pvv5-dt','pvv10-dt','pvv10+-dt']
+    rdf = pd.DataFrame(mylist,columns=cols)
+    rdf.to_csv('results.csv',index=False)
+
+if __name__ == "__main__":
+    myfolder = sys.argv[1]
+    main(myfolder)
+
+"""
+docker run -it -u $(id -u):$(id -g) -w $PWD pangyuteng/ml:latest bash
+
+
+"""
