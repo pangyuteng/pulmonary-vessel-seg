a72a96f4d69dac1e035bb3d075ae3401c23011da
pangyuteng <pangyuteng@gmail.com>
2023-10-14 13:50:28 -0700

    wip

--- pvv/scale-space-particle/run.sh
+++ pvv/scale-space-particle/run.sh
@@ -7,4 +7,8 @@ cd /cvibraid/cvib2/Temp/ptengtmp
 #ConvertDicom --dir dicom -o CT.nrrd
 #GenerateMedianFilteredImage -i CT.nrrd -o CT-median.nrrd
 #GeneratePartialLungLabelMap --ict CT-median.nrrd -o partialLungLabelMap.nrrd
-python /ChestImagingPlatform/Scripts/cip_compute_vessel_particles.py -i CT.nrrd -l partialLungLabelMap.nrrd -r WholeLung -r LeftLung -r RightLung -o particles.vtk --perm -s 0.625 --tmpDir=$workdir
+
+python /ChestImagingPlatform/Scripts/cip_compute_vessel_particles.py \
+    -i CT.nrrd -l partialLungLabelMap.nrrd \
+    -r WholeLung -r LeftLung -r RightLung -o particles.vtk \
+    --perm -s 0.625 --tmpDir=/cvibraid/cvib2/Temp/ptengtmp
